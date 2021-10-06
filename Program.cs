6063bd3c716cc43239ddf6a0239544e774b4ee22
parth_shel <ps@parthshel.com>
2021-10-06 03:20:18 -0400

    added driver script

--- Program.cs
+++ Program.cs
@@ -8,15 +8,48 @@ namespace SimaticLibrary
         {
             Console.WriteLine("Hello World!");
 
-            try
+            bool done = false;
+            do
             {
-                SimaticString ss = new SimaticString();
-                Console.WriteLine($"max length: {ss.MaxLength} - length: {ss.Length}");
+                try
+                {
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    Console.Write("enter Simatic string (or empty string to give up): ");
+                    string s = Console.ReadLine();
+
+                    if (String.IsNullOrEmpty(s))
+                    {
+                        done = true;
+                    }
+
+                    Console.WriteLine("creating simatic string...");
+
+                    SimaticString ss = new SimaticString(s.Length);
+
+                    Console.WriteLine($"max length: {ss.MaxLength} - length: {ss.Length}");
+
+                    Console.WriteLine("populating simatic string...");
+
+                    ss.Set(s);
+
+                    Console.WriteLine($"max length: {ss.MaxLength} - length: {ss.Length}");
+                    Console.WriteLine($"string representation: '{ss.ToString()}'");
+                    Console.WriteLine($"byte representation: {BitConverter.ToString(ss.GetBytes())}");
+
+                    Console.WriteLine("reversing simatic string...");
+
+                    ss.Reverse();
+
+                    Console.WriteLine($"max length: {ss.MaxLength} - length: {ss.Length}");
+                    Console.WriteLine($"string representation: '{ss.ToString()}'");
+                    Console.WriteLine($"byte representation: {BitConverter.ToString(ss.GetBytes())}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception Caught! - {ex.Message}");
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception Caught! - {ex.Message}");
-            }
+            while (!done);
         }
     }
 }
