2194c4143026947228af46d1459df219aa91f99b
parth_shel <ps@parthshel.com>
2021-10-06 00:00:17 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimaticLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
