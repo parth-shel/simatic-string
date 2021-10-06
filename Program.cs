7a1b1883f4ad04879ceed4a8ca6382286b14e2f7
parth_shel <ps@parthshel.com>
2021-10-06 02:05:09 -0400

    added simple driver script to test class implementation

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ namespace SimaticLibrary
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            try
+            {
+                SimaticString ss = new SimaticString();
+                Console.WriteLine($"max length: {ss.MaxLength} - length: {ss.Length}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Caught! - {ex.Message}");
+            }
         }
     }
 }
