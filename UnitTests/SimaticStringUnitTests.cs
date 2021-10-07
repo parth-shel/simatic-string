fc215110a39ed34929d00d0ef4c8eb81dc8ebb2d
parth_shel <ps@parthshel.com>
2021-10-07 01:30:20 -0400

    added unit tests

--- UnitTests/SimaticStringUnitTests.cs
+++ UnitTests/SimaticStringUnitTests.cs
@@ -12,8 +12,95 @@ namespace UnitTests
         {
             SimaticString ss = new SimaticString();
 
-            Xunit.assert(ss.Length == 0);
-            Xunit.assert(ss.MaxLength == 254);
+            Assert.Equal(0, ss.Length);
+            Assert.Equal(254, ss.MaxLength);
+
+            SimaticString sn = new SimaticString(10);
+
+            Assert.Equal(0, sn.Length);
+            Assert.Equal(10, sn.MaxLength);
+        }
+
+        [Fact]
+        public void ConstructAndSetTest()
+        {
+            SimaticString ss = new SimaticString(5);
+
+            Assert.Equal(5, ss.MaxLength);
+            Assert.Equal(0, ss.Length);
+
+            ss.Set("Hello");
+
+            Assert.Equal(5, ss.Length);
+            Assert.Equal(5, ss.MaxLength);
+
+            byte[] arr = ss.GetBytes();
+            Assert.Equal(Convert.ToByte(5) ,arr[0]);
+            Assert.Equal(Convert.ToByte(5), arr[1]);
+            Assert.Equal(Convert.ToByte('H'), arr[2]);
+            Assert.Equal(Convert.ToByte('e'), arr[3]);
+            Assert.Equal(Convert.ToByte('l'), arr[4]);
+            Assert.Equal(Convert.ToByte('l'), arr[5]);
+            Assert.Equal(Convert.ToByte('o'), arr[6]);
+            Assert.Equal("Hello", ss.ToString());
+
+            SimaticString sn = new SimaticString(ss);
+
+            Assert.Equal(5, sn.MaxLength);
+            Assert.Equal(5, sn.Length);
+
+            byte[] byteArray = {Convert.ToByte('W'), Convert.ToByte('o'), Convert.ToByte('r'), Convert.ToByte('l'), Convert.ToByte('d')};
+            sn.Set(byteArray);
+
+            Assert.Equal(5, sn.MaxLength);
+            Assert.Equal(5, sn.Length);
+
+            arr = sn.GetBytes();
+            Assert.Equal(Convert.ToByte(5) ,arr[0]);
+            Assert.Equal(Convert.ToByte(5), arr[1]);
+            Assert.Equal(Convert.ToByte('W'), arr[2]);
+            Assert.Equal(Convert.ToByte('o'), arr[3]);
+            Assert.Equal(Convert.ToByte('r'), arr[4]);
+            Assert.Equal(Convert.ToByte('l'), arr[5]);
+            Assert.Equal(Convert.ToByte('d'), arr[6]);
+
+            sn.Set(ss);
+
+            Assert.Equal(5, sn.MaxLength);
+            Assert.Equal(5, sn.Length);
+
+            arr = sn.GetBytes();
+            Assert.Equal(Convert.ToByte(5) ,arr[0]);
+            Assert.Equal(Convert.ToByte(5), arr[1]);
+            Assert.Equal(Convert.ToByte('H'), arr[2]);
+            Assert.Equal(Convert.ToByte('e'), arr[3]);
+            Assert.Equal(Convert.ToByte('l'), arr[4]);
+            Assert.Equal(Convert.ToByte('l'), arr[5]);
+            Assert.Equal(Convert.ToByte('o'), arr[6]);
+        }
+
+        [Fact]
+        public void ReverseTest()
+        {
+            SimaticString ss = new SimaticString();
+            ss.Set("Harshdeep");
+
+            Assert.Equal(9, ss.Length);
+
+            byte[] arr = ss.GetBytes();
+            Assert.Equal(Convert.ToByte(254) ,arr[0]);
+            Assert.Equal(Convert.ToByte(9), arr[1]);
+
+            Assert.Equal("Harshdeep", ss.ToString());
+            ss.Reverse();
+
+            arr = ss.GetBytes();
+            Assert.Equal(Convert.ToByte(254) ,arr[0]);
+            Assert.Equal(Convert.ToByte(9), arr[1]);
+
+            Assert.Equal(9, ss.Length);
+
+            Assert.Equal("peedhsraH", ss.ToString());
         }
     }
 }
